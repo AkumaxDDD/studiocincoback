e0d99b23596a2b1318f3abf094af2c1ec3994643
AkumaxDDD.gaspar.ahumada101@gmail.com <AkumaxDDD>
2024-12-13 16:44:14 -0300

    dbzz

--- db.js
+++ db.js
@@ -1,15 +1,11 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
-export const conectarDB = async () => {
-  const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10, // Ajusta según tus necesidades
-    queueLimit: 0,
-  });
+export const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
   try {
     const connection = await pool.getConnection();
@@ -20,5 +16,4 @@ export const conectarDB = async () => {
     throw error;
   }
 
-  return pool;
-};
+
