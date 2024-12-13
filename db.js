6255c327a6f48bcfd0c95ca35b3d230df2983d88
AkumaxDDD.gaspar.ahumada101@gmail.com <AkumaxDDD>
2024-12-13 16:49:14 -0300

    apa

--- db.js
+++ db.js
@@ -1,19 +1,12 @@
 import mysql from "mysql2/promise";
 
-export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+export let db;
 
-  try {
-    const connection = await pool.getConnection();
-    console.log('Conexión a la base de datos establecida');
-    connection.release();
-  } catch (error) {
-    console.error('Error al conectar con la base de datos:', error);
-    throw error;
-  }
-
-
+export async function conectarDB() {
+  db = await mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  });
+}
