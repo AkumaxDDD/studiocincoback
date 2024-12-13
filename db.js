efb64cd1329527ed585a401a827c86bf97cc2eff
AkumaxDDD.gaspar.ahumada101@gmail.com <AkumaxDDD>
2024-12-13 17:25:40 -0300

    connection

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 export let db;
 
 export async function conectarDB() {
-  db = await mysql.createPool({
+  db = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
