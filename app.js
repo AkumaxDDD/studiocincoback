dcbbeab3cda0185c5819fad49c92f8208e99d68c
AkumaxDDD.gaspar.ahumada101@gmail.com <AkumaxDDD>
2024-12-13 17:32:59 -0300

    pool

--- app.js
+++ app.js
@@ -17,10 +17,29 @@ app.use(cors({
   origin: 'https://dashboard.render.com/', // o '*' para permitir todas las fuentes
 }));
 
-app.get("/", (req, res) => {
-  res.send("Hola mundo!");
+app.get("/", async (req, res) => {
+  const [apiactividades] = await db.execute(`
+    SELECT DISTINCT
+      a.id_actividad AS id,
+      a.nombre AS actividad, 
+      u.nombre AS instructor, 
+      GROUP_CONCAT(DISTINCT p.dias ORDER BY p.dias SEPARATOR ', ') AS dias, 
+      GROUP_CONCAT(DISTINCT h.horario ORDER BY h.horario SEPARATOR ', ') AS horarios, 
+      GROUP_CONCAT(DISTINCT c.costo ORDER BY c.costo SEPARATOR ', ') AS costos,
+      -- Convertir la imagen a base64
+      IFNULL(TO_BASE64(a.imagen), '') AS imagen
+    FROM actividades a
+    JOIN usuario_actividad ua ON a.id_actividad = ua.id_actividad
+    JOIN usuarios u ON ua.id_usuario = u.id_usuario
+    JOIN actividad_horarios ah ON a.id_actividad = ah.id_actividad
+    JOIN planes p ON ah.id_plan = p.id_plan
+    JOIN horarios h ON ah.id_horario = h.id_horario
+    JOIN costos c ON ah.id_costo = c.id_costo
+    GROUP BY a.id_actividad, a.nombre, u.nombre, a.imagen
+    ORDER BY a.nombre, u.nombre;
+  `);
+  res.json({ actividad: apiactividades[0] });
 });
-
 app.use("/api/actividades", actividadesRouter);
 
 app.listen(port, () => {
--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 export let db;
 
 export async function conectarDB() {
-  db = await mysql.createConnection({
+  db = await mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
